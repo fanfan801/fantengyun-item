3e1c95c30fafd5ebff220c546de8a71937a75bc6
fantengyun <you@example.com>
2020-12-10 10:32:28 +0800

    第一次提交

---
+++ src/script/list_module.js
@@ -0,0 +1,32 @@
+define(['jlazyload'], () => {
+    return {
+        init: function() {
+            const $list = $('.list ul');
+            //1.渲染list.html页面
+            $.ajax({
+                url: 'http://localhost/dashboard/JS2010/week06/Day%2029-Day%2031_jquery/projectname/php/listdata.php',
+                dataType: 'json'
+            }).done(function(data) {
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    $strhtml += `
+                        <li>
+                            <a href="detail.html?sid=${value.sid}">
+                            <img class="lazy" data-original="${value.url}" width="200" height="200"/>
+                                <p>${value.title}</p>
+                                <span>￥${value.price}</span>
+                            </a>
+                        </li>
+                    `;
+                });
+                $list.html($strhtml);
+                $(function() { //页面加载完成
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //显示方法：谈入
+                    });
+                });
+            });
+
+        }
+    }
+})
